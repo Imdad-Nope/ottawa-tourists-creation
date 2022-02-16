0fc68224db611de5a370795918190ced9755de3c
Imdad-Nope <mdalauddinovi62@gmail.com>
2022-02-16 09:50:11 +0600

    ok

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -3,17 +3,23 @@ import banner from '../../images/bannerOt.jpg'
 import './Banner.css';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
 
 const Banner = () => {
     return (
-        <Card sx={{ maxWidth: '100%' }}>
-            <CardMedia
-                component="img"
-                height="70%"
-                image={banner}
-                alt="green iguana"
-            />
-        </Card>
+
+
+        <Grid item xs={12} sm={12} md={12} >
+            <Card sx={{ maxWidth: '100%' }}>
+                <CardMedia
+                    component="img"
+                    height="70%"
+                    image={banner}
+                    alt="green iguana"
+                />
+            </Card>
+        </Grid>
+
     );
 };
 
