33f1a2ad157df2784ca090ce875302ddaa171cf6
Imdad-Nope <mdalauddinovi62@gmail.com>
2022-02-16 23:52:32 +0600

    amended

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -2,21 +2,14 @@ import React from 'react';
 import banner from '../../images/bannerOt.jpg'
 import './Banner.css';
 import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
-
 const Banner = () => {
     return (
-
-
-        <Grid item xs={12} sm={12} md={12} >
-            <Card sx={{ maxWidth: '100%' }}>
-                <CardMedia
-                    component="img"
-                    height="70%"
-                    image={banner}
-                    alt="green iguana"
-                />
+        <Grid spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Card>
+                <img
+                    style={{ width: '100%' }}
+                    src={banner} alt="" />
             </Card>
         </Grid>
 
